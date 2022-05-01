a9860f557df7584bad4188d67c19b430b5ccc631
Grégory Clain <g.clain69@gmail.com>
2022-05-01 14:50:01 +0200

    add msg to html

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("un utilisateur s'est connecté");
+  socket.on("chat message", (msg) => {
+    console.log("message", msg);
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3000, () => {
